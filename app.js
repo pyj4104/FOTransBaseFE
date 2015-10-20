9cf629712eed8bdf4c44f3fadf2ee57c732217ea
YeounJun Park <pyj4104@hotmail.com>
2015-10-20 12:57:05 -0700

    Initial setup.

---
+++ app.js
@@ -0,0 +1,16 @@
+(function() {
+var AppComponent = ng
+	.Component({
+		selector: "my-app",
+		template: "<p class=\"test\">tesaoehuntoehusnacuh.ntuheonthusaoethunsaoheuntoeasncuhaoenstubscnaoeuhaosehu tnhueonahucnh ntshauntsaeobntt</p>"
+	})
+	.Class({
+		constructor: function(){
+
+		}
+	})
+
+document.addEventListener('DOMContentLoaded', function() {
+	ng.bootstrap(AppComponent);
+});
+})();
