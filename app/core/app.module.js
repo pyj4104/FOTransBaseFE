e44cf710e89900686deefe8d8743f8abd983c396
YeounJun Park <pyj4104@hotmail.com>
2015-11-02 09:13:45 -0800

    Update on app.module.

---
+++ app/core/app.module.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// Declare all the modules
+angular.module('Authentication', []);
+
+// Declare the main
+angular.module("FOTransBaseFE", [
+	'ngRoute',
+	'ngCookies',
+	'ngResource',
+	'Authentication']);
